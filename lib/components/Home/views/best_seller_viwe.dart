0987f43f1eb9dccdb7dd68137073690640b91fea
elsaramji <elsaramegymahmoud404@gmail.com>
2025-02-05 13:49:03 +0200

    add products to best seller

--- lib/components/Home/views/best_seller_viwe.dart
+++ lib/components/Home/views/best_seller_viwe.dart
@@ -1,5 +1,6 @@
 // components/Home/views/best_seller_viwe.dart
 import 'package:flutter/material.dart';
+
 import '../../../core/custom/widgets/CustomHome/custom_Page_Appbar.dart';
 import '../../../core/styles/color_style.dart';
 import '../../../core/styles/font_style.dart';
@@ -18,19 +19,23 @@ class _BestSellerViweState extends State<BestSellerViwe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(slivers: [
-          const SliverToBoxAdapter(
-            child: CustomPageAppbar(pagetitel: "الأكثر مبيعًا"),
-          ),
-          SliverToBoxAdapter(
-            child: Text(
-              "الأكثر مبيعًا",
-              style: TextsStyle.bold16.copyWith(color: AppColors.grayscale950),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        child: SafeArea(
+          child: CustomScrollView(slivers: [
+            const SliverToBoxAdapter(
+              child: CustomPageAppbar(pagetitel: "الأكثر مبيعًا"),
             ),
-          ),
-          const ProductsSreamBuilder(),
-        ]),
+            SliverToBoxAdapter(
+              child: Text(
+                "الأكثر مبيعًا",
+                style:
+                    TextsStyle.bold16.copyWith(color: AppColors.grayscale950),
+              ),
+            ),
+            const ProductsSreamBuilder(),
+          ]),
+        ),
       ),
     );
   }
